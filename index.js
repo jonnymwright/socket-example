c1cecca8244b3639c587e2bd5650a6a06e5908ef
Jonny Wright <jmwright@scottlogic.co.uk>
2018-11-14 15:45:06 +0000

    Add basic sockets support

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var app = require("express")();
 var http = require("http").Server(app);
+var io = require('socket.io')(http);
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(4000, function() {
-  console.log("listening on *:4000");
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const port = 4000;
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
